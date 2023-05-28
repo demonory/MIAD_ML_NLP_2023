abf9f12d1fd71f9b1a0596dcc0dcb4d4e451d368
demonory <129700342+demonory@users.noreply.github.com>
2023-05-27 21:06:04 -0500

    Update api.py

--- model_deployment_project_2/api.py
+++ model_deployment_project_2/api.py
@@ -93,7 +93,7 @@ def predict_gender(Year,Title,Plot):
     cols = ['p_Action', 'p_Adventure', 'p_Animation', 'p_Biography', 'p_Comedy', 'p_Crime', 'p_Documentary', 'p_Drama', 'p_Family',
         'p_Fantasy', 'p_Film-Noir', 'p_History', 'p_Horror', 'p_Music', 'p_Musical', 'p_Mystery', 'p_News', 'p_Romance',
         'p_Sci-Fi', 'p_Short', 'p_Sport', 'p_Thriller', 'p_War', 'p_Western']
-    res = pd.DataFrame([resultado], columns=cols)
+    res = dict(zip(cols,resultado))
     return res
 
 # In[ ]:
